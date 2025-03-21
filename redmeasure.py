3ccf04d2de0a89cd07ea0da7824b6255054c5cf0
eduardocardenasv27 <a00232432@tec.mx>
2025-03-21 01:20:35 -0600

    Update redmeasure.py

--- redmeasure.py
+++ redmeasure.py
@@ -10,27 +10,35 @@ import matplotlib.pyplot as plt
 import cv2
 import numpy as np
 
-
-image = cv2.imread('C:\\Users\\Edgardo\\Pictures\\gasatest2.jpg')
+image = cv2.imread('C:\\Users\\Edgardo\\Pictures\\gasatest3.jpg')
 
 def findredp(image):
     red_channel = image[:, :, 2]
     green_channel = image[:, :, 1]
     blue_channel = image[:, :, 0]
-
+    adjusted_red_channel = red_channel - green_channel - blue_channel
+    adjusted_red_channel = np.clip(adjusted_red_channel, 0, 255)
+    adjusted_red_value = np.sum(adjusted_red_channel)
     red_value = np.sum(red_channel)
     green_value = np.sum(green_channel)
     blue_value = np.sum(blue_channel)
-
+    
     totalrgb = red_value + green_value + blue_value
-    redpercent = red_value / totalrgb
-    print(redpercent)
+
+    redpercent = adjusted_red_value / totalrgb
+    print("Red percent:", redpercent)
+
     greenpercent = green_value / totalrgb
-    print(greenpercent)
+    print("Green percent:", greenpercent)
+    
     bluepercent = blue_value / totalrgb
-    print(bluepercent)
+    print("Blue percent:", bluepercent)
 
     return redpercent
 
 redpercentage = findredp(image)
-print(redpercentage*30)
+print("SATURACION DE GASA: ", redpercentage*30, " ml")
+
+cv2.imshow("Ejemplo", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()

671d30087cf62b0e8e596ef1506eb79c6a3b85bc
eduardocardenasv27 <a00232432@tec.mx>
2025-03-23 17:41:24 -0600

    Added comments to Hemomeasure_interfazV2.py

--- Hemomeasure_interfazV2.py
+++ Hemomeasure_interfazV2.py
@@ -11,16 +11,19 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-
+#Funcion para aumentar brillantez de imagen dada una imagen y el factor de brillantez por el cual se multiplicara.
+#Calcula la intensidad promedio de cada pixel, multiplica la imagen por el factor de brillantez ingresado por el usuario y luego le suma el promedio al resultado.
 def adjust_brilliance(image, brilliance_factor=1.5):
-    image = image.astype(np.float32)
-    mean_brightness = np.mean(image)
+    image = image.astype(np.float32) 
+    mean_brightness = np.mean(image) 
     image = (image - mean_brightness) * brilliance_factor + mean_brightness
     image = np.clip(image, 0, 255)
     image = image.astype(np.uint8)
 
     return image
 
+#Ajusta la exposicion de una imagen dada la imagen y un factor de exposicion de parte del usuario.
+#Multiplica la imagen por el factor de exposicion.
 def adjust_exposure(image, exposure_factor=1.5):
     image = image.astype(np.float32)
 
@@ -31,6 +34,9 @@ def adjust_exposure(image, exposure_factor=1.5):
     image = image.astype(np.uint8)
     return image
 
+
+#Busca el porcentaje de rojo de una imagen dada una imagen.
+#Suma los valores R, G y B de todos los pixeles de la imagen, los suma y le resta el total de pixeles verdes y azules al total de pixeles rojos, para despues dividir el numero entre el total de pixeles de la imagen.
 
 def findredp(image):
     height, width, _ = image.shape
@@ -66,10 +72,15 @@ def findredp(image):
     
     return redpercentage
 
+
 def hemomeasure(image, brilliance_factor=5, exposure_factor=3, blur_ksize=(123, 123)):
+    #Ajusta brillo y exposicion para hacer los blancos mas blancos y los rojos mas rojos.
     image = adjust_brilliance(image, brilliance_factor)
     image = adjust_exposure(image, exposure_factor)
+    #Aplica un efecto de difuminado para reducir el nivel de detalles en la imagen que puedan obstruir en la identificacion de colores, como la textura de la tela.
     image = cv2.GaussianBlur(image, blur_ksize, 0)
+
+    #Calcula el porcentaje de rojo de la imagen ya procesada.
     redpercentage = findredp(image)
     return redpercentage
 
@@ -90,14 +101,14 @@ def llamar():
     image = cv2.imread(fn)
     image2 = cv2.imread(fn2)
     
-    #Optimizacion
+    #Reduce el tamaño de la imagen en caso de que sea muy grande para facilitar la carga y evitar posibles complicaciones en caso de que la imagen sea muy grande.
     if (image.shape[0]*image.shape[1])>100:
         image = cv2.resize(image, (int(image.shape[1] * 0.25), int(image.shape[0] * 0.25)))
 
     if (image2.shape[0]*image2.shape[1])>100:
         image2 = cv2.resize(image2, (int(image2.shape[1] * 0.25), int(image2.shape[0] * 0.25)))
     
-    #LLama a la funcion con la imagen
+    #LLama a la funcion con la imagen para calcular sus porcentajes de rojo
     print("LADO A")
     print("////////////")
     redpercentage = hemomeasure(image, brilliance_factor=5, exposure_factor=3, blur_ksize=(123, 123))
@@ -106,6 +117,8 @@ def llamar():
     print("////////////")
     redpercentage2 = hemomeasure(image2, brilliance_factor=5, exposure_factor=3, blur_ksize=(123, 123))
 
+    #Multiplica ambos porcentajes de rojo por la mitad de la maxima retencion de sangre de una gaza de 10cm y las suma entre si para aproximar la cantidad de sangre en la gaza.
+    
     print("")
     print("SATURACION ESTIMADA EN GAZA: ", (redpercentage*6)+(redpercentage2*6),"ml")
 
